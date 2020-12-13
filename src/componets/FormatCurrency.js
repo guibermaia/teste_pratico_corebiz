b1381a09480b9b17d26639709a4777ad9d90b862
guibermaia <guilhermebertoldi21384@gmail.com>
2020-12-12 21:08:55 -0300

    card structure, rendering the cards

---
+++ src/componets/FormatCurrency.js
@@ -0,0 +1,6 @@
+export const SYMBOL_BRL = 'R$';
+
+export const format = (value, symbol) => {
+    const parsedValue = isNaN(value) ? 0 : parseFloat(value, 10);
+    return `${symbol} ${parsedValue.toFixed(2)}`;
+}
--- src/componets/Home/Home.js
+++ src/componets/Home/Home.js
@@ -1,67 +1,107 @@
+import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
-import React from 'react';
-import { MdSearch, MdPerson, MdShoppingCart, MdEmail, MdHeadsetMic } from "react-icons/md";
+import {
+    MdSearch,
+    MdPerson,
+    MdShoppingCart,
+    MdEmail,
+    MdHeadsetMic,
+    MdChevronLeft,
+    MdChevronRight,
+    MdStar,
+    MdStarBorder
+} from "react-icons/md";
 
 import './home.css';
+import { SYMBOL_BRL, format } from '../FormatCurrency';
 
 function Home() {
-
+    const [product, setProduct] = useState([]);
+    
     function getProducts() {
         Axios.get('https://corebiz-test.herokuapp.com/api/v1/products').then(result => {
-            console.log(result);
+            setProduct(result.data);
         }).catch(err => {
             console.error(err);
         })
     }
-    // getProducts();
-    return (
-        <div>
-            <header>
-                <p>COREBIZ</p>
-                <div>
-                    <input type="text" placeholder="O que está procurando? " />
-                    <MdSearch />
-                </div>
-                <div className="content-div-header">
-                    <p><MdPerson /> Minha conta</p>
-                    <MdShoppingCart />
-                </div>
-            </header>
-            <div className="carousel">
-                <h1>img carrosel</h1>
-            </div>
-            <div className="products">
-                <div className="card-product">
-                </div>
-            </div>
 
-            <form>
-                <h3>Participe de nossas news com promoções e novidades</h3>
-                <input type="text" placeholder="Digite seu nome" />
-                <input type="text" placeholder="Digite seu e-mail" />
-                <button>EU QUERO!</button>
-            </form>
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(e)
+    }
 
-            <footer>
-                <div>
-                    <h3>Localização</h3>
-                    <p className="div-trace"></p>
-                    <p>Avenida Andrômeda, 2000. Bloco 6 e 8</p>
-                    <p>Alphavile SP</p>
-                    <p>brasil@corebiz.ag</p>
-                    <p>+55 11 3090 1039</p>
+    function renderStar(product) {
+        for (let i = 0; i <= product; i++) {
+            return <MdStar />
+        }
+    }
+
+    useEffect(() => {
+        getProducts();
+    }, []);
+
+    if (product) {
+        return (
+            <div>
+                <header>
+                    <div className="div-container-header">
+                        <p>COREBIZ</p>
+                        <div>
+                            <input type="text" placeholder="O que está procurando? " />
+                            <MdSearch />
+                        </div>
+                        <div className="content-div-header">
+                            <p><MdPerson /> Minha conta</p>
+                            <MdShoppingCart />
+                        </div>
+                    </div>
+                </header>
+                <div className="carousel"></div>
+                <div className="products">
+                    <MdChevronLeft className="size-svg" />
+                    {product.map((item) => (
+                        <div key={item.productId} className="card-product">
+                            <img src={item.imageUrl} alt="Produto"></img>
+                            {renderStar(item.stars)}
+                            <p>{item.productName}</p>
+                            <p>por: {format(item.price, SYMBOL_BRL)}</p>
+                        </div>
+                    ))}
+                    <MdChevronRight className="size-svg" />
                 </div>
-                <div className="content-div-footer">
-                    <button> <MdEmail /> ENTRE EM CONTATO</button>
-                    <button> <MdHeadsetMic /> FALE COM O NOSSO CONSULTOR ONLINE</button>
+
+                <form>
+                    <div>
+                        <h3>Participe de nossas news com promoções e novidades!</h3>
+                        <input type="text" placeholder="Digite seu nome" name="userName" />
+                        <input type="text" placeholder="Digite seu e-mail" name="email" />
+                        <button onClick={handleSubmit}>EU QUERO!</button>
+                    </div>
+                </form>
+
+                <footer>
+                    <div>
+                        <h3>Localização</h3>
+                        <p className="div-trace"></p>
+                        <p>Avenida Andrômeda, 2000. Bloco 6 e 8</p>
+                        <p>Alphavile SP</p>
+                        <p>brasil@corebiz.ag</p>
+                        <p>+55 11 3090 1039</p>
+                    </div>
+                    <div className="content-div-footer">
+                        <button> <MdEmail /> ENTRE EM CONTATO</button>
+                        <button> <MdHeadsetMic /> FALE COM O NOSSO CONSULTOR ONLINE</button>
+                    </div>
+                    <div>
+                        imagem
+                        imagem
                 </div>
-                <div>
-                    imagem
-                    imagem
-                </div>
-            </footer>
-        </div>
-    );
+                </footer>
+            </div >
+        );
+    }
+    return <div className="loading"><div className="loader"></div></div>
 };
 
 export default Home;
